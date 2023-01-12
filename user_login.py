7bf21653d32b31aac8177da1ff2b4f8de9279803
Fatih Özaydın <122316778+ozaydinfatihim@users.noreply.github.com>
2023-01-12 05:55:28 +0300

    Update user_login.py

--- user_login.py
+++ user_login.py
@@ -1,20 +1,20 @@
 print("""********************
-Kullanici Girişi:
+User Login:
 ********************
     """)
 
-sys_kullanici_adi = ("fatih")
-sys_parola = "123456"
+sys_user_name = ("fatih")
+sys_password = "123456"
 
-kullanici_adi = input("Kullanici Adi: ")
-parola = input("Parola: ")
+user_name = input("User_Name: ")
+password = input("Password: ")
 
 
-if (sys_kullanici_adi == kullanici_adi and sys_parola != parola):
-    print("Parola Hatali...")
-elif (sys_kullanici_adi != kullanici_adi and sys_parola == parola):
-    print("Kullanici Adi Hatali...")
-elif (sys_kullanici_adi != kullanici_adi and sys_parola != parola):
-    print("Kullanici Adi ve Parola Hatali...")
+if (sys_user_name == user_name and sys_password != password):
+    print("The Password Is Incorrect...")
+elif (sys_user_name != user_name and sys_password == password):
+    print("The Username Is Incorrect...")
+elif (sys_user_name != user_name and sys_password != password):
+    print("Username and Password is incorrect...")
 else:
-    print("Sisteme Giriş Başariyla Yapildi...")
+    print("System Login Successfully...")
